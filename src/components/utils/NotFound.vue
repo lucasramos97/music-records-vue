65a25d02a087c92a0fc6b742e4cd48e35d282d13
lucasramos97 <lucasramosdev@gmail.com>
2021-11-25 15:35:09 -0300

    Added not found screen

---
+++ src/components/utils/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>404 not found!</h1>
+  <div style="text-align: center">
+    <a href="/musics" class="link">Click back</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Login from '@/components/users/Login.vue';
 import MusicList from '@/components/musics/MusicList.vue';
 import DeletedMusicList from '@/components/musics/DeletedMusicList.vue';
+import NotFound from '@/components/utils/NotFound.vue';
 import AuthenticationService from '@/services/AuthenticationService';
 
 const routes: Array<RouteRecordRaw> = [
@@ -22,6 +23,10 @@ const routes: Array<RouteRecordRaw> = [
     path: '/login',
     component: Login,
   },
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFound,
+  },
 ];
 
 const router = createRouter({
@@ -31,7 +36,8 @@ const router = createRouter({
 
 const authenticationService = new AuthenticationService();
 router.beforeEach((to, from, next) => {
-  if (to.path !== '/login' && !authenticationService.isAuthenticated())
+  if (to.matched && to.matched[0].path === '/:pathMatch(.*)*') next();
+  else if (to.path !== '/login' && !authenticationService.isAuthenticated())
     next({ path: '/login' });
   else next();
 });
